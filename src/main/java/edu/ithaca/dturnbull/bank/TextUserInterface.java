2c5fdcba93ad96ddf85e9a318b87b63072a00d32
mikolajkusa <mkonieczny@ithaca.edu>
2022-02-26 08:55:17 -0500

    Full implemented text user interface

--- src/main/java/edu/ithaca/dturnbull/bank/TextUserInterface.java
+++ src/main/java/edu/ithaca/dturnbull/bank/TextUserInterface.java
@@ -7,63 +7,189 @@ package edu.ithaca.dturnbull.bank;
  * @date 2/25/2022
  */
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class TextUserInterface {
     public static void main(String[] args){
         
+        //login
         Scanner myObj = new Scanner(System.in);
-        Random rand = new Random();
-        System.out.println("Create bank customer account:");
-        //set up screen
-        System.out.println("Please enter your first name:");
-        String firstName = myObj.next();
-        System.out.println("Please enter your last name:");
-        String lastName = myObj.next();
-        System.out.println("Please enter your id:");
-        int id = myObj.nextInt();
-        System.out.println("Please enter your email:");
-        String email = myObj.next();
+        BankCustomer client = new BankCustomer("Benedict Reginald","McDonald",12345678,"reggi89@gmail.com",1231422);
+        SavingsAccount savings = new SavingsAccount(0.5, 1500);
+        CheckingAccount checking = new CheckingAccount(500);
+        client.addCheckingAccount(checking);
+        client.addSavingsAccount(savings);
+        System.out.println("Welcome back, Benedict Reginald McDonald!");
+        
 
-        BankCustomer user = new BankCustomer(firstName, lastName, rand.nextInt(10000000,99999999),email,id);
-        System.out.println("User added successfully!");
-        
-        //add account screen
-        System.out.println("Please add a bank account. Choose 1 for checking or 2 for savings:");
-        int userSelection = myObj.nextInt();
-        while(userSelection != 1 || userSelection != 2){
-            System.out.println("Invalid input. Choose 1 for checking or 2 for savings:");
-            userSelection = myObj.nextInt();
-        }
-        if(userSelection==1){
-            System.out.println("Please choose a starting balance:");
-            double balance = myObj.nextDouble();
-            user.addCheckingAccount(new CheckingAccount(balance));
-        }
-        else if(userSelection==1){
-            System.out.println("Please choose a starting balance:");
-            double balance = myObj.nextDouble();
-            user.addSavingsAccount(new SavingsAccount(0.5,balance));
-        }
-        System.out.println("Success!");
-        
         //main screen
-        while (userSelection != 11){
-            
-            System.out.println("What would you like to do? Please enter your selection:");
-            System.out.println("1 - add new checking account");
-            System.out.println("2 - add new savings account");
-            System.out.println("3 - deposit funds to your checking account");
-            System.out.println("4 - deposit funds to your savings account");
-            System.out.println("5 - withdraw funds from your checking account");
-            System.out.println("6 - withdraw funds from your savings account");
-            System.out.println("7 - get balance from checking account");
-            System.out.println("8 - get balance from savings account");
-            System.out.println("9 - transfer funds from checking to savings account");
-            System.out.println("10 - transfer funds from savings to checking account");
-            System.out.println("11 - log out");
+        int userSelection = 0;
+        while(userSelection!=3){
+            System.out.println("Welcome to the main screen. Please enter your selection:");
+            System.out.println("1 - Access your checking account");
+            System.out.println("2 - Access your savings account");
+            System.out.println("3 - Log out");
+            userSelection = myObj.nextInt();
+            while(userSelection>3 || userSelection<0){
+                System.out.println("Please re-enter your selection.");
+                userSelection = myObj.nextInt(); 
+            }
+            //checking account screen   
+            if(userSelection == 1){
+                while(userSelection != 5){
+                    System.out.println("Welcome to the checking account screen. Please enter your selection:");
+                    System.out.println("1 - Check current balance");
+                    System.out.println("2 - Deposit money");
+                    System.out.println("3 - Withdraw funds");
+                    System.out.println("4 - Transfer funds to savings account");
+                    System.out.println("5 - Return to main screen");
+                    userSelection = myObj.nextInt();
+
+                    while(userSelection>5 || userSelection<0){
+                        System.out.println("Please re-enter your selection.");
+                        userSelection = myObj.nextInt(); 
+                    }
+
+                    if(userSelection == 1){
+                        System.out.println("Your current checking balance is $" + client.getCheckingBalance());
+                    }
+                    if(userSelection == 2){
+                        System.out.println("Please enter amount to be deposited:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.depositChecking(amount);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }  
+                    }
+                    if(userSelection == 3){
+                        System.out.println("Please enter amount to be withdrawn:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.withdrawChecking(amount);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }
+                        catch(InsufficientFundsException error){
+                            System.out.println(error);
+                        }
+                    }
+                    if(userSelection == 4){
+                        System.out.println("Please enter amount to be transferred:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.transferChecking(amount, savings);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }
+                        catch(InsufficientFundsException error){
+                            System.out.println(error);
+                        }
+                    }
+                    
+                }
+
+
+            }
+            //savings account screen
+            else if(userSelection == 2){
+                while(userSelection != 5){
+                    System.out.println("Welcome to the savings account screen. Please enter your selection:");
+                    System.out.println("1 - Check current balance");
+                    System.out.println("2 - Deposit money");
+                    System.out.println("3 - Withdraw funds");
+                    System.out.println("4 - Transfer funds to checking account");
+                    System.out.println("5 - Return to main screen");
+                    userSelection = myObj.nextInt();
+
+                    while(userSelection>5 || userSelection<0){
+                        System.out.println("Please re-enter your selection.");
+                        userSelection = myObj.nextInt(); 
+                    }
+
+                    if(userSelection == 1){
+                        System.out.println("Your current savings balance is $" + client.getSavingsBalance());
+                    }
+                    if(userSelection == 2){
+                        System.out.println("Please enter amount to be deposited:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.depositSavings(amount);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }  
+                    }
+                    if(userSelection == 3){
+                        System.out.println("Please enter amount to be withdrawn:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.withdrawSavings(amount);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }
+                        catch(InsufficientFundsException error){
+                            System.out.println(error);
+                        }
+                    }
+                    if(userSelection == 4){
+                        System.out.println("Please enter amount to be transferred:");
+                        double amount = myObj.nextDouble();
+                        try{
+                            client.transferSavings(amount, checking);
+                            System.out.println("Success!");
+                        }
+                        catch(IllegalArgumentException error){
+                            System.out.println(error);
+                        }
+                        catch(FrozenException error){
+                            System.out.println(error);
+                        }
+                        catch(InsufficientFundsException error){
+                            System.out.println(error);
+                        }
+                    } 
+                }
+
+                
+            }
         }
+
+        
+        System.out.println("Have a good day!");
+        myObj.close();
+    
+        
+
+        
+
+            
+
+        
 
 
 
